08d32d1371b2ca841cff93c2c460c4e35b9066dc
Asim Bera <asimbera@outlook.in>
2021-08-09 15:45:14 +0530

    Fixed slideshow

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
 //<-------------navbar-links color change------------->
 
 $(window).scroll(function () {
-    if ($(this).scrollTop() >= 100) {
-        $('nav').addClass('nav-fixed');
-    } else {
-        $('nav').removeClass('nav-fixed');
-    }
+  if ($(this).scrollTop() >= 100) {
+    $('nav').addClass('nav-fixed');
+  } else {
+    $('nav').removeClass('nav-fixed');
+  }
 });
-
 
 // ,<-------------nav-header color change-------------->
 
-$(document).ready(function(){       
-    var scroll_pos = 0;
-    $(document).scroll(function() { 
-        scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 100) {
-            $('.nav-header').css('color', '#000');
-        } else {
-            $('.nav-header').css('color', '#fff');
-        }
-    });
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 100) {
+      $('.nav-header').css('color', '#000');
+    } else {
+      $('.nav-header').css('color', '#fff');
+    }
+  });
 });
-
-
 
 //<-----------carouserl slider------------->
 
 // $('.autoplay').owlCarousel({
 //     loop:true,
 //     margin:10,
-    
+
 //     dots: true,
 //     autoplay: true,
 //     autoplayTimeout:2000,
@@ -47,36 +44,38 @@ $(document).ready(function(){
 //     }
 // });
 
-document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '#card-slider', {
-		perPage    : 3,
-		breakpoints: {
-			600: {
-				perPage: 1,
-			}
-		}
-	} ).mount();
-} );
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('#card-slider', {
+    // cover: true,
+    height: '600px',
+    gap: '5px',
+    perPage: 3,
+    breakpoints: {
+      600: {
+        perPage: 1,
+      },
+    },
+  }).mount();
+});
 
 //top to scroll button
 
 /*scroll to top*/
-$(document).ready(function(){
-$(window).scroll(function(){
-    if($(this).scrollTop() > 100) {
-        $("#arrow i").fadeIn();
-}
-    else {
-        $("#arrow i").fadeOut();
-}
-
-});
-$("#arrow i").on('click',function(){
-$("html,body").animate({
-    scrollTop: 0
-}, 600);
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#arrow i').fadeIn();
+    } else {
+      $('#arrow i').fadeOut();
+    }
+  });
+  $('#arrow i').on('click', function () {
+    $('html,body').animate(
+      {
+        scrollTop: 0,
+      },
+      600
+    );
     return false;
+  });
 });
-
-});
-
